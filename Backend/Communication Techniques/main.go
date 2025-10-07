9874b1ca00445889b87df897f17b995edd108c6e
absolutenaman <absolute.naman@gmail.com>
2025-10-07 15:48:36 +0530

    Added long polling and fixed short pollinh logic

---
+++ Backend/Communication Techniques/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	server := gin.Default()
+	server.Handle("GET", "/getData", HandleShortPolling)
+	server.Handle("GET", "/UpdateData", UpdatePollingData)
+	err := server.Run("localhost:8000")
+	if err != nil {
+		panic(err)
+	}
+}
+
+var data string
+var clientsArr []int64
+
+func initialiseDummyValue() {
+	data = "initial data"
+}
+func HandleShortPolling(ctx *gin.Context) {
+	initialiseDummyValue()
+	lastName := ctx.Query("lastName")
+	if lastName != "" {
+	} else {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.JSON(http.StatusOK, gin.H{"data": data})
+	}
+}
+func UpdatePollingData(ctx *gin.Context) {
+	data = "Updated Data"
+	ctx.Header("Access-Control-Allow-Origin", "*")
+
+	ctx.JSON(http.StatusOK, gin.H{"data": data})
+}
